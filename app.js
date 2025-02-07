28a207850f95132c07e80d20abe80bba21c84f18
Sarbjot Singh <diamondsardarji@gmail.com>
2025-02-07 13:14:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+
+app.get("/",(req,res)=>{
+    res.send("hi i am root");
+})
+
+app.listen(8080,()=>{
+    console.log("listening port at 8080");
+})
